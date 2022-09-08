9818b601868bcb3cddca18073b8d53620e76bbac
Isabella López <101228522+Isabellalps@users.noreply.github.com>
2022-09-08 10:47:32 -0500

    Create variables2.py

---
+++ variables2.py
@@ -0,0 +1,23 @@
+#Día 2 de los 30 dias de programación con Python
+
+from tkinter.messagebox import YES
+
+
+is_married = "yes"
+is_true = "no"
+is_light_on = "yes"
+today = this_day = "Thursday"
+
+print(type(is_married))
+
+print("Hello, what's your name? ")
+name = input("My name is: ")
+print("Nice to meet you: " , name)
+
+print("And your last name is? ")
+last_name = input("My last name is: ")
+print("Your last name is: ", last_name)
+
+print("Your full name is: ", name, last_name)
+
+
